b9d150041e5c65b8f1f151254b8816c032efc081
Jakub <jakub.bchyla@gmail.com>
2022-11-02 14:25:54 +0100

    Initial commit

---
+++ src/main/java/com/amigo2021/AngularSpringBootAmigo2021/AngularSpringBootAmigo2021Application.java
@@ -0,0 +1,13 @@
+package com.amigo2021.AngularSpringBootAmigo2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootAmigo2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootAmigo2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amigo2021/AngularSpringBootAmigo2021/AngularSpringBootAmigo2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigo2021.AngularSpringBootAmigo2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootAmigo2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
